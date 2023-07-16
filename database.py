e20f6fc0cb610cc8e599d9fbb20265d8f240399e
Alexander <firefighter-911@rambler.ru>
2023-07-16 23:28:10 +0300

    Телефонный справочник

---
+++ database.py
@@ -0,0 +1,56 @@
+def write_name(name):
+    with open("telephone.txt", "a", encoding="UTF-8") as file:
+        file.write(name)
+
+def search_data(char):
+    count = 0
+    lst_find_man = []
+    with open("telephone.txt", "r", encoding="UTF-8") as file:
+        lst = file.readlines()
+        for row in lst:
+            if char in row:
+                count += 1
+                print(f"{count}) {row.strip()}")
+                lst_find_man.append(row)
+    return lst_find_man
+
+def change_data(old_data, new_data):
+    with open("telephone.txt", "r", encoding="UTF-8") as file:
+        lst = file.readlines()
+    with open("telephone.txt", "w", encoding="UTF-8") as file:
+        for row in lst:
+            if row == old_data:
+                file.write(new_data)
+            else:
+                file.write(row)
+
+def delete_data(old_data):
+    with open("telephone.txt", "r", encoding="UTF-8") as file:
+        lst = file.readlines()
+    with open("telephone.txt", "w", encoding="UTF-8") as file:
+        for row in lst:
+            if row == old_data:
+                continue
+            else:
+                file.write(row)
+
+def sort(num):
+    with open("telephone.txt", "r", encoding="UTF-8") as file:
+        lst = file.readlines()
+    if num == 2:
+        lst.sort(key=lambda x: x.split(",")[1])
+    elif num == 3:
+        lst.sort(key=lambda x: x.split(",")[2])
+    elif num == 4:
+        lst.sort(key=lambda x: x.split(",")[3])
+    elif num == 5:
+        lst.sort(key=lambda x: x.split(",")[4])
+    else:
+        lst.sort(key=lambda x: int(x.split(",")[num-1]))
+    with open("telephone.txt", "w", encoding="UTF-8") as file:
+        lst = file.writelines(lst)
+
+def view_all():
+    with open("telephone.txt", "r", encoding="UTF-8") as file:
+        print(file.read())
+
